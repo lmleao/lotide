4bb24d354fb8c6b5395d397477eeac588551299a
Luiza Leao <luiza.leao96@gmail.com>
2023-10-22 17:15:02 -0400

    Made console.log messages more consistent.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,9 +20,9 @@ const eqArrays = function(arr1, arr2) {
 //It should take the results from eqArrays and log an appropriate message
 const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed! ${array1} is not equal to ${array2}.`);
+    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
   } else {
-    console.log(`✅✅✅ Assertion passed! ${array1} is equal to ${array2}`);
+    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
   }
 };
 
