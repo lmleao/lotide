4ea9d969998a6a9929c0f1f40c6c382f0f215f3a
Luiza Leao <luiza.leao96@gmail.com>
2023-10-29 01:06:54 -0400

    Add eqArrays function to code.

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  //First compare array lengths
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  //Loop through each array and compare each index
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; //If any elements don't match, return false
+    }
+  }
+
+  //If all elements match, return true
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
