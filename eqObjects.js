5e3bcc2886026c1468ce25c60ca0c79d7073f48e
Luiza Leao <luiza.leao96@gmail.com>
2023-10-29 01:16:28 -0400

    Add condition in eqObjects function to check for arrays.

--- eqObjects.js
+++ eqObjects.js
@@ -34,7 +34,11 @@ const eqObjects = function(object1, object2) {
   }
 
   for (let key of objKeys1) {
-    if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -56,7 +60,7 @@ assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), false);
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
