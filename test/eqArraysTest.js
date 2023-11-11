74d2f61b225232d04f79303859ce268c4f264c56
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 18:52:29 -0500

    Make test cases with M&C.

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-//Test Cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe("#eqArrays", () => {
+  it("compares arrays", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("works with nested arrays", () => {
+    assert.deepEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+});
