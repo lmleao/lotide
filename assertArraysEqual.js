9825b712ff93eb1a1ca72ca9cdb1aa8236605d52
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 13:03:43 -0500

    Enhance code readability.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,9 +4,9 @@ const eqArrays = require('./eqArrays');
 //It should take the results from eqArrays and log an appropriate message
 const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
+    console.log(`❌❌❌ Assertion failed: ${array1} !== ${array2}.`);
   } else {
-    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}.`);
   }
 };
 
