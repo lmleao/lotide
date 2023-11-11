d75936fe1c34461d0f888543491e0b730ad1bdb0
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 16:37:25 -0500

    Refactor code.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,41 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  //First compare array lengths
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  //Loop through each array and compare each index
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false; //If any elements don't match, return false
-    }
-  }
-
-  //If all elements match, return true
-  return true;
-};
-
-const eqObjects = function(object1, object2) {
-  const objKeys1 = Object.keys(object1);
-  const objKeys2 = Object.keys(object2);
-
-  if (objKeys1.length !== objKeys2.length) {
-    return false;
-  }
-
-  for (let key of objKeys1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -47,5 +10,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-assertObjectsEqual({ color: "red", size: "medium" }, { color: "red", size: "medium"});
-assertObjectsEqual({ color: "pink", items: 20 }, { color: "pink", items: 20 });
+module.exports = assertObjectsEqual;
