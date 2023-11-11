1353323e0a0d5389fd01fc85c587988b8f1e728b
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 17:31:31 -0500

    Refactor code.

--- letterPositions.js
+++ letterPositions.js
@@ -1,31 +1,3 @@
-//eqArrays function
-const eqArrays = function(arr1, arr2) {
-  //First compare array lengths
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  //Loop through each array and compare each index
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false; //If any elements don't match, return false
-    }
-  }
-
-  //If all elements match, return true
-  return true;
-};
-
-//Implement assertArraysEqual function
-//It should take the results from eqArrays and log an appropriate message
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
-  } else {
-    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
-  }
-};
-
 const letterPositions = function(sentence) {
   const results = {};
   
@@ -44,9 +16,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-//Test cases
-assertArraysEqual(letterPositions("hello").e, [1]);
-
-const result = letterPositions('lighthouse in the house');
-assertArraysEqual(result['l'], [0]);
-assertArraysEqual(result['i'], [1, 11]);
+module.exports = letterPositions;
