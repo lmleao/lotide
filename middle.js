98313369732e50f1854581275cf9bd285cdbbd18
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 13:09:13 -0500

    Refactor code.

--- middle.js
+++ middle.js
@@ -1,31 +1,3 @@
-//eqArrays function
-const eqArrays = function(arr1, arr2) {
-  //First compare array lengths
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  //Loop through each array and compare each index
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false; //If any elements don't match, return false
-    }
-  }
-
-  //If all elements match, return true
-  return true;
-};
-
-//Implement assertArraysEqual function
-//It should take the results from eqArrays and log an appropriate message
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
-  } else {
-    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
-  }
-};
-
 //Implement middle function
 //It should return the middle element of an array
 //If the array only has 2 elements, it returns an empty array
@@ -46,7 +18,4 @@ const middle = function(array) {
   return newArray;
 };
 
-//Test Cases to ensure function runs as expected
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
