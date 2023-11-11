16ad3927c0e1526d0741e1ad1b6c4287d55087c4
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 17:36:40 -0500

    Refactor code.

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
 
@@ -10,28 +8,4 @@ const map = function(array, callback) {
   return results;
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
-  } else {
-    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
-  }
-};
-
-assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
-assertArraysEqual(map(words, word => word[2]), ['o', 'n', undefined, 'j', 'm']);
-assertArraysEqual(map(words, word => word[1]), ['r', 'o', 'o', 'a', 'o']);
+module.exports = map;
