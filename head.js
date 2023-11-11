f03118c60ffe2bfce13685ecbb7e1463399faafa
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 12:41:35 -0500

    Refactor code.

--- head.js
+++ head.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Function to return first item in an array
 const head = function(array) {
   return array[0];
 };
 
-// Test functions
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
