ec898d3c344893d771675f8aaac0ad2be09604e4
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 14:01:32 -0500

    Add another test case.

--- test/middleTest.js
+++ test/middleTest.js
@@ -13,4 +13,11 @@ describe("#middle", () => {
   it("returns empty array if original array only has 2 elements", () => {
     assert.deepEqual(middle([1, 2]), []);
   });
+
+  it("make sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
+  });
 });
