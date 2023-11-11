e30d72b265c1b73580ac1a4b576f6cb95b8c7882
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 13:30:41 -0500

    Change tests to utilize M&C.

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,28 @@
-// test/headTest.js
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// Test functions
-// Test case: Check the first element of an array of numbers
-assertEqual(head([5,6,7]), 5);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-// Test case: Check the first element of an array of strings
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
 
-// Test case: Check the first element of an array with a single number
-assertEqual(head([1]), 1);
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
 
-// Test case: Check the first element of an empty array
-assertEqual(head([]), undefined);
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
