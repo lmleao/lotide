7cd2f089b116a2b8db9252f3ab8038fe2b838dec
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 12:44:41 -0500

    Add comments to test cases.

--- test/headTest.js
+++ test/headTest.js
@@ -4,7 +4,14 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 // Test functions
+// Test case: Check the first element of an array of numbers
 assertEqual(head([5,6,7]), 5);
+
+// Test case: Check the first element of an array of strings
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// Test case: Check the first element of an array with a single number
 assertEqual(head([1]), 1);
+
+// Test case: Check the first element of an empty array
 assertEqual(head([]), undefined);
