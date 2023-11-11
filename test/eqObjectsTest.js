ca05716922037dc6f4f37651b9d63a3665628c60
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 16:50:09 -0500

    Create test file for eqObjects function.

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const eqObjects = require('../eqObjects');
+
+describe("#eqObjects", () => {
+  it("compares objects as equal", () => {
+    const shirtObject = { color: "red", size: "medium" };
+    const anotherShirtObject = { size: "medium", color: "red" };
+    assert.deepEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  });
+
+  it("returns false if objects don't have the same number of keys", () => {
+    const shirtObject = { color: "red", size: "medium" };
+    const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+    assert.deepEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  });
+
+  it("compares keys which have an Array as a value", () => {
+    const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+    const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+    assert.deepEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+  });
+});
