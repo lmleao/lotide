773dcecf8f01388c6e884bf63e00da8d4f23b3e9
Luiza Leao <luiza.leao96@gmail.com>
2023-11-11 17:49:06 -0500

    Refactor code.

--- without.js
+++ without.js
@@ -1,31 +1,3 @@
-//eqArrays function
-const eqArrays = function(arr1, arr2) {
-  //First compare array lengths
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  //Loop through each array and compare each index
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false; //If any elements don't match, return false
-    }
-  }
-
-  //If all elements match, return true
-  return true;
-};
-
-//assertArraysEqual function
-//It should take the results from eqArrays and log an appropriate message
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion failed: ${array1} is not equal to ${array2}.`);
-  } else {
-    console.log(`✅✅✅ Assertion passed: ${array1} is equal to ${array2}.`);
-  }
-};
-
 // The without function makes a new array when provided with
 // two arrays passed as arguments, the first serving as the source array
 // and the second serving as the items to be removed from the source array.
@@ -42,17 +14,4 @@ const without = function(source, itemsToRemove) {
   return newArray;
 };
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-// Test Cases: Checks if the returned element arrays are correct
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // should PASS
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // should PASS
-assertArraysEqual(without([1, 3, 4], [1, 4, 6, 7]), [3]); // should PASS
-assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]); // should PASS
-
-// Test Case: Checks if the original array has been modified by the without function
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
